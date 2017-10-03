84088c97d431594601458dbbb23f5670b57c3715
sajingeo <sajin.geo@gmail.com>
2017-10-03 15:11:18 -0400

    Initial working copy.

---
+++ skimmerscanner/README.md
@@ -0,0 +1,6 @@
+##iOS app to detect HC-05 credit card skimmers on iOS
+
+##CREDITS TO
+https://github.com/sparkfunX/Skimmer_Scanner
+https://medium.com/@yostane/getting-started-with-bluetooth-low-energy-on-ios-ada3090fc9cc
+https://wingoodharry.wordpress.com/2016/01/21/get-list-of-ble-devices-using-corebluetooth-on-ios-swift/
--- skimmerscanner/ViewController.swift
+++ skimmerscanner/ViewController.swift
@@ -1,25 +1,59 @@
-//
-//  ViewController.swift
-//  skimmerscanner
-//
-//  Created by Sajin George on 10/3/17.
-//  Copyright © 2017 Sajin George. All rights reserved.
-//
-
+import CoreBluetooth
 import UIKit
 
 class ViewController: UIViewController {
-
+    var centralManager: CBCentralManager?
+    var peripherals = Array<CBPeripheral>()
+    
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Initialise CoreBluetooth Central Manager
+        centralManager = CBCentralManager(delegate: self, queue: DispatchQueue.main)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
+extension ViewController: CBCentralManagerDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if (central.state == .poweredOn){
+            self.centralManager?.scanForPeripherals(withServices: nil, options: nil)
+        }
+        else {
+            // do something like alert the user that ble is not on
+        }
+    }
+    
+    func show_alert(){
+        let alertController = UIAlertController(title: "Skimmer detected", message: "Please inform local authorities!!!", preferredStyle: .alert)
+        
+        let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(defaultAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        peripherals.append(peripheral)
+        tableView.reloadData()
+        if (peripheral.name == "HC-05")
+        {
+            show_alert()
+        }
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier: "cell")! as UITableViewCell
+        
+        let peripheral = peripherals[indexPath.row]
+        cell.textLabel?.text = peripheral.name
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return peripherals.count
+    }
+}
